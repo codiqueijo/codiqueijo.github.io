db837963b501ec803f274d87e24b7d9dcc16dafd
Thamara Andrade <tkcandrade@gmail.com>
2020-09-30 01:22:50 -0300

    Notícias

--- assets/js/data/cache-list.js
+++ assets/js/data/cache-list.js
@@ -32,7 +32,7 @@ const include = [
   /* Tabs */
   {% for tab in site.data.tabs %}
     {% capture item %}
-      {%- unless tab.name == 'Home' -%}
+      {%- unless tab.name == 'Notícias' -%}
         /tabs/{{ tab.name | downcase }}
       {%- endunless -%}
       {{- "/" -}}
